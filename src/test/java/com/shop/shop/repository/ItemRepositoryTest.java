ce4f9095ee3be5722a311c965207da0ec1efc235
unknown <glgkwls1@naver.com>
2023-02-06 17:27:30 +0900

    [feat]:@Query annotaion

--- src/test/java/com/shop/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/shop/repository/ItemRepositoryTest.java
@@ -40,7 +40,7 @@ class ItemRepositoryTest {
             Item item = new Item();
             item.setItemNm("테스트 상품" + i);
             item.setPrice(10000 + i);
-            item.setItemDetail("테스트 상품 상세 설명 + i");
+            item.setItemDetail("테스트 상품 상세 설명" + i);
             item.setItemSellStatus(ItemSellStatus.SELL);
             item.setStockNumber(100);
             item.setRegTime(LocalDateTime.now());
@@ -81,7 +81,7 @@ class ItemRepositoryTest {
     }
 
     @Test
-    @DisplayName("가격 내링차순 조회 테스트")
+    @DisplayName("가격 내림차순 조회 테스트")
     public void findByPriceLessThanOrderByPriceDescTest(){
         this.createItemList();
         List<Item> itemList =
@@ -91,4 +91,24 @@ class ItemRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for(Item item  : itemList){
+            System.out.println(item.toString());
+
+        }
+    }
+
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 }
