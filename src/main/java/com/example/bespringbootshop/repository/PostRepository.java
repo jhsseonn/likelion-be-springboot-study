56e0264e1bef9f3ad171a1f2e6b1b06ed01e7328
jhsseonn <hs125bt@gmail.com>
2023-02-08 20:32:49 +0900

    fix: (PostRepository) 한꺼번에 함수 추가시 테이블 생성 오류로 잠시 주석처리

--- src/main/java/com/example/bespringbootshop/repository/PostRepository.java
+++ src/main/java/com/example/bespringbootshop/repository/PostRepository.java
@@ -1,17 +1,19 @@
 package com.example.bespringbootshop.repository;
 
 import com.example.bespringbootshop.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface PostRepository {
-    List<Post> findByPostTitle(String title);
-    List<Post> findByPostTitleOrContent(String title, String content);
-    List<Post> findByCheckLessThan(Integer check);
-    List<Post> findByCheckLessThenOrderByDesc(Integer check);
+public interface PostRepository extends JpaRepository<Post, Long>{
+//    List<Post> findByPostTitle(String postTitle);
+//    List<Post> findByPostTitleOrContent(String postTitle, String postContent);
+//    List<Post> findByViewLessThan(Integer postView);
+//    List<Post> findByViewLessThenOrderByDesc(Integer postView);
 
-    @Query("select i from Post i where i.content like %:content% order by i.check desc")
-    List<Post> findByPostContent(@Param("content") String content);
+//    @Query("select i from Post i where i.postContent like %:content% order by i.postView desc")
+//    List<Post> findByPostContent(@Param("postContent") String postContent);
 }
