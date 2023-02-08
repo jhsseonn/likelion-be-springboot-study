dcd3b5d038127db60a8608054ea3121e50d30edb
jhsseonn <hs125bt@gmail.com>
2023-02-08 19:39:37 +0900

    feat: (Post) post 엔티티 맵핑 추가

--- src/main/java/com/example/bespringbootshop/entity/Post.java
+++ src/main/java/com/example/bespringbootshop/entity/Post.java
@@ -4,22 +4,36 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table("post")
+@Table(name = "post")
 @Getter
 @Setter
 @ToString
 public class Post {
+
+    @Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private String writer;
+
+    @Column(nullable = false, length=50)
     private String title;
+
+    @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false)
     private int check;
+
+    @Column(nullable = false)
     private int wish;
+
     private LocalDateTime regTime;
 
     private LocalDateTime updateTime;
