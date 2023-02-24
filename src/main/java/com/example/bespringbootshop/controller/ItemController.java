7ef439e42a7d51aca15cbe446d31d2cba6ad56ca
jhsseonn <hs125bt@gmail.com>
2023-02-24 20:55:33 +0900

    fix: (ItemController) 오류 수정

--- src/main/java/com/example/bespringbootshop/controller/ItemController.java
+++ src/main/java/com/example/bespringbootshop/controller/ItemController.java
@@ -14,30 +14,11 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
 public class ItemController {
 
-    private final ItemService itemService;
-
     @GetMapping(value="/admin/item/new")
-    public String itemForm(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, Model model, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList){
-
-        if(bindingResult.hasErrors()){
-            return "item/itemForm";
-        }
-
-        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId()==null){
-            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값입니다.");
-            return "item/itemForm";
-        }
-
-        try{
-            itemService.saveItem(itemFormDto, itemImgFileList);
-        }catch (Exception e){
-            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
-            return "item/itemForm";
-        }
-
-        return "redirect:/";
+    public String itemForm(Model model){
+        model.addAttribute("itemFormDto", new ItemFormDto());
+        return "item/itemForm";
     }
 }
