165e74512590346bac3457ae1a38993afbafbb57
jhsseonn <hs125bt@gmail.com>
2023-03-01 19:16:12 +0900

    fix: (ItemService) 상품 수정하기 기능 추가

--- src/main/java/com/example/bespringbootshop/service/ItemService.java
+++ src/main/java/com/example/bespringbootshop/service/ItemService.java
@@ -1,15 +1,18 @@
 package com.example.bespringbootshop.service;
 
 import com.example.bespringbootshop.dto.ItemFormDto;
+import com.example.bespringbootshop.dto.ItemImgDto;
 import com.example.bespringbootshop.entity.Item;
 import com.example.bespringbootshop.entity.ItemImg;
 import com.example.bespringbootshop.repository.ItemImgRepository;
 import com.example.bespringbootshop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -36,5 +39,20 @@ public class ItemService {
         }
 
         return item.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId){
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg:itemImgList){
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
     }
 }
