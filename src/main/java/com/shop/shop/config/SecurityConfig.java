8b5f88dcb2c7e706bbf7ba333327444cfe50b85e
unknown <glgkwls1@naver.com>
2023-03-06 18:37:37 +0900

    [feat]:페이지 권한 설정

--- src/main/java/com/shop/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/shop/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,14 +26,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // http 요청에 대한 보안 설정
         http.formLogin()
-                .loginPage("/member/login")
+                .loginPage("/members/login")
                 .defaultSuccessUrl("/")
                 .usernameParameter("email")
                 .failureUrl("/members/login/error")
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/")
+        ;
+
+        http.authorizeRequests() // 시큐리티 처리에 HttpServletRequest를 이용하겠음
+                .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ;
+
+        // 인증되지 않은 사용자가 리소스에 접근하면 수행되는 핸들러 등록
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+        ;
     }
 
     @Bean
@@ -48,6 +61,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // userDetailService 구현 객체로 memberService를 지정해주며, 비밀번호 암호를 위해 passwordEncoder 지정
         auth.userDetailsService(memberService)
                 .passwordEncoder(passwordEncoder());
+    }
+
+    // static 디렉터리의 하위 파일은 인증을 무시하도록 설정
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
 }
